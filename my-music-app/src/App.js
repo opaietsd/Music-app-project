92d5c925e87319d1577aada689f2423bdc346d1d
Dariia <opaetsd@gmail.com>
2022-09-09 23:07:31 +0300

    finished the header part(and style)

--- my-music-app/src/App.js
+++ my-music-app/src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import companyLogo from './assets/imgs/logo.svg';
+import circle from'./assets/imgs/circle.svg';
 
 function App() {
   return (
-    <div className="App">
-   
+    <div className="app">
+        <header className="app-header">
+          <nav className='app-nav'>
+            <div className='app-nav-logo'>
+              <img src={ companyLogo } ></img>
+              <p>Soundify</p>
+            </div>
+            <ul className='app-nav-ul'>
+              <li><a href=''>Discover</a></li>
+              <li><a href=''>Join</a></li>
+            </ul>
+          </nav>
+          <div className='app-header-info'>
+            <h1>Feel the Music</h1>
+            <p>Stream over 20,000 tracks with one click</p>
+            <button>Stream Now</button>
+          </div>
+          <div>
+            <img src={ circle } className='firstCircle'></img>
+            <img src={ circle } className='secondCircle'></img>
+            <img src={ circle } className='thirdCircle'></img>
+          </div>
+      </header>
     </div>
   );
 }

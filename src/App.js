949a747634e913069d1e37a07a3d6f9dd3eb4d22
Dariia <opaetsd@gmail.com>
2022-09-29 21:02:12 +0300

    changed the router type

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./Components/About";
 import Menu from "./Components/Menu";
 import Popular from "./Components/Popular";
 import Evolve from "./Components/Evolve";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {HashRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (

ac9ed695622cc124f8b1cf46593ca9043595dc12
Dariia <opaetsd@gmail.com>
2022-09-29 22:23:00 +0300

    worked on the album Evolve page

--- src/Components/Evolve.js
+++ src/Components/Evolve.js
@@ -3,12 +3,76 @@ import React from "react";
 function Evolve(){
     return (
         <div className="EvolveAlbum">
-            <div className="evolveCover">
+            <header className="evolveCover">
                 <h1>Evolve</h1>
-            </div>
-            <div>
+            </header>
 
-            </div>
+                <div className="EvolveTeam">
+                    <div className="team-container">
+                    <h2>Productive</h2>
+                    <h2>Team</h2>
+                    </div>
+                 <table className="Team-table">
+                        <tr>
+                            <th>Team</th>
+                            <th>Motion designer</th>
+                            <th>Product Managers</th>
+                            <th>Music</th>
+                        </tr>
+                        <tr>
+                            <td>Imagine Dragons</td>
+                            <td>Jenny Wilson</td>
+                            <td>Theresa Webb</td>
+                            <td>Darlene Robertson</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Cameron Williamson</td>
+                            <td>Floyd Miles</td>
+                            <td>Leslie Alexander</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Brooklyn Simmons</td>
+                            <td>Darlene Robertson</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td>Marvin McKinney</td>
+                        </tr>
+                        <tr>
+                            <th>Producer</th>
+                            <th>Band members</th>
+                            <th>Backend Developers</th>
+                            <th>Stylist</th>
+                        </tr>
+                        <tr>
+                            <td>Ilya Bregman</td>
+                            <td>Dan Reynolds</td>
+                            <td>Arlene McCoy</td>
+                            <td>Arlene McCoy</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Daniel Platzman</td>
+                            <td>Courtney Henry</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Ben McKee</td>
+                            <td>Bessie Cooper</td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td>Daniel Wayne</td>
+                        </tr>
+                 </table>
+             </div>
+             <div className="slick">
+
+             </div>
+
         </div>
     )
 }

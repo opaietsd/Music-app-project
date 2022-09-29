48f9fd99e012a7a46064fe69b499f2689f8d98ab
ermiahp <ermiahasanpour@gmail.com>
2022-09-29 19:28:50 +0200

    Fix lint of the files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import './App.css';
-import HomePage from './Components/Home';
-import About from './Components/About';
-import Menu from './Components/Menu';
-import Popular from './Components/Popular';
-import Evolve from './Components/Evolve'
-import {BrowserRouter as Router, Routes, Route } from  'react-router-dom';
-
+import "./App.css";
+import HomePage from "./Components/Home";
+import About from "./Components/About";
+import Menu from "./Components/Menu";
+import Popular from "./Components/Popular";
+import Evolve from "./Components/Evolve";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-    return (
-     <Router>
-        <div>
-          <Routes>
-              <Route path='/' exact element={<HomePage />}/>
-              <Route path='/about' element={<About/>}/>
-              <Route path='/menu' element={<Menu />}/>
-              <Route path='/popular' element={<Popular />}/>
-              <Route path='/evolve' element={<Evolve />}/>
-          </Routes>
-        </div>
-      </Router>
-    );
-  }
-  
-  export default App;
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/popular" element={<Popular />} />
+          <Route path="/evolve" element={<Evolve />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
